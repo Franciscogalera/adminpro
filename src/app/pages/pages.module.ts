aefa3a906180b9ac4a194584cf08b2a50a332661
Francisco Galera <franciscogalera@MacBook-Pro-2.local>
2022-07-17 03:02:06 +0200

    Add input Output Progress

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
-import { AppRoutingModule } from '../app-routing.module';
+
 
 
 
@@ -14,17 +20,20 @@ import { AppRoutingModule } from '../app-routing.module';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    AppRoutingModule
+    PagesComponent,
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent]
+    PagesComponent,
+  ],
+  imports: [ 
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    RouterModule,
+    ComponentsModule
+  ]
 })
 export class PagesModule { }
